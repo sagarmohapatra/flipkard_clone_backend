87c33a118e7ddef90e8d6866b8b14c610215119b
sagarmohapatra <sagarkumarmohapatra5@gamail.com>
2023-01-03 12:55:30 +0530

    new saga

--- router/router.js
+++ router/router.js
@@ -1,9 +1,10 @@
 import express from "express";
 const router = express.Router();
 import { userSignup, userlogin } from "../controller/user_controller";
-import { getProducts } from "../controller/product_controller"
+import { getProducts, getProductByid } from "../controller/product_controller"
 
 router.post("/signup", userSignup)
 router.post("/login", userlogin)
 router.get("/products", getProducts)
+router.get("/product/:id", getProductByid)
 export default router;
