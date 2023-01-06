a0dcfa12d21358f061648b4cc1f46cdb1096c9e8
sagarmohapatra <sagarkumarmohapatra5@gamail.com>
2023-01-06 13:20:52 +0530

    push code

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ dotenv.config()
 import router from "./router/router";
 import cors from "cors"
 import bodyParser from "body-parser";
+import { v4 as uuid } from "uuid"
 // const USERNAME = process.env.DB__USERNAME
 // const PASSWORD = process.env.DB_PASSWORD
+
 app.use(cors())
 
 app.use(bodyParser.json({ extended: true }))
@@ -34,5 +36,22 @@ const PORT = 8000
 app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`))
 
 DefaultData();
+
+
+
+export let paytmMerchantKey = process.env.PAYTM_MERCHANT_KEY;
+export let paytmParams = {};
+paytmParams["MID"] = process.env.PAYTM_MID;
+paytmParams["WEBSITE"] = process.env.PAYTM_WEBSITE;
+paytmParams["CHANNEL_ID"] = process.env.PAYTM_CHANNEL_ID;
+paytmParams["INDUSTY_TYPE_ID"] = process.env.PAYTM_INDUSTRY_TYPE_ID;
+paytmParams["ORDER_ID"] = uuid();
+paytmParams["CUST_ID"] = process.env.PAYTM_CUST_ID;
+paytmParams["TXN_AMOUNT"] = "100";
+paytmParams["CALLBACK_URL"] = "http://localhost:8000/callback";
+paytmParams["EMAIL"] = "sagarkumarmohapatra5@gmail.com";
+paytmParams["MOBIL_NO"] = "9777937220";
+
+
 
 
